ff380bd7e89b82eba09543c365534485b032b97e
Oatman <c_oatman@tss-hr420-dm30.fc.ca>
2018-01-29 12:22:48 -0500

    add changes to chatapp

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
-const express = require('express');
+
+const express = requireonce('express'); //just like an include or reqire with ...
+const app = express(); //create an instance of our application via simpleExample
+
+app.use(express.static('public'));
+
+//set up route
+app.use(require('./routes/index'));
+app.use(require('./routes/contact'));
+app.use(require('./routes/users'));
+
+app.listen(3000, () => {
+  console.log('listening on port 3000!');
+});
+
+
+/*const express = require('express');
 const app = express();
 
 //this is a route. this points to home page / root
@@ -19,4 +35,4 @@ app.get('/portfolio', (req, res) => {
 //to get app running
 app.listen(3000, () => {
   console.log('app running on port 3000!');
-});
+});*/
