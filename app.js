2e9160ca1997618757b90d18573416e010f7f1cb
chanteloatman <31936830+chanteloatman@users.noreply.github.com>
2018-01-29 15:48:35 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,35 @@
-const express = require('express');
+
+const express = require('express'); //just like an include or reqire with ...
+const app = express(); //create an instance of our application via simpleExample
+const io = require('socket.io')();
+
+
+app.use(express.static('public'));
+
+//set up routes
+app.use(require('./route/index'));
+app.use(require('./route/contact'));
+app.use(require('./route/users'));
+
+const server = app.listen(3000, () => {
+  console.log('listening on port 3000!');
+});
+
+io.attach(server);
+
+io.on('connection', (socket) => {
+  console.log('a user has connected!');
+  io.emit('connectMsg', { for: 'everyone', msg : `${socket.id} is here`});
+
+socket.on('disconnect', () => {
+  console.log('a user has disconnected!');
+});
+
+});
+
+
+
+/*const express = require('express');
 const app = express();
 
 //this is a route. this points to home page / root
@@ -19,4 +50,4 @@ app.get('/portfolio', (req, res) => {
 //to get app running
 app.listen(3000, () => {
   console.log('app running on port 3000!');
-});
+});*/
