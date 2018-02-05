c606cdd9d6dbbc0a8909ec2b2d9083b13132d742
chanteloatman <31936830+chanteloatman@users.noreply.github.com>
2018-02-05 13:25:40 -0500

    add week 3 changes

--- app.js
+++ app.js
@@ -1,4 +1,42 @@
-const express = require('express');
+
+const express = require('express'); //just like an include or reqire with ...
+const app = express(); //create an instance of our application via simpleExample
+const io = require('socket.io')();
+
+
+app.use(express.static('public'));
+
+//set up routes
+app.use(require('./route/index'));
+app.use(require('./route/contact'));
+app.use(require('./route/users'));
+
+const server = app.listen(3000, () => {
+  console.log('listening on port 3000!');
+});
+
+io.attach(server);
+
+io.on('connection', (socket) => {
+  console.log('a user has connected!');
+  io.emit('chat message', { for: 'everyone', message : `${socket.id} is here`});
+
+  //handle messages sent from the client
+  socket.on('chat message', msg => {
+  io.emit('chat message', { for: 'everyone', message : msg});
+});
+
+socket.on('disconnect', () => {
+  console.log('a user has disconnected!');
+
+  io.emit('disconnect message', `${socket.id} has left the building!`);
+});
+
+});
+
+
+
+/*const express = require('express');
 const app = express();
 
 //this is a route. this points to home page / root
@@ -19,4 +57,4 @@ app.get('/portfolio', (req, res) => {
 //to get app running
 app.listen(3000, () => {
   console.log('app running on port 3000!');
-});
+});*/
